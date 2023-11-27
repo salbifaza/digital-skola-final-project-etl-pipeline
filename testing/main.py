f6e486f955c7b319b013a39f00ee13dbae0c2b33
salbifaza <salbifaza13@gmail.com>
2023-11-28 00:07:06 +0700

    update testing

--- testing/main.py
+++ testing/main.py
@@ -1,7 +1,6 @@
 from modules.fetch_api import FetchApi
 from modules.transform import Transform
 from sqlalchemy import create_engine
-import logging
 
 def connect_mysql(user,password,host,db,port):
     engine = create_engine("mysql+mysqlconnector://{}:{}@{}:{}/{}".format(
@@ -21,6 +20,7 @@ mysql_login    = 'mysql'
 mysql_password = 'mysql'
 mysql_schema   = 'mysql'
 mysql_port     = 3307
+
 engine_mysql = connect_mysql(
     host     = mysql_host,
     user     = mysql_login,
@@ -28,25 +28,12 @@ engine_mysql = connect_mysql(
     db       = mysql_schema,
     port     = mysql_port
 )
-
 
 postgre_host      = '127.0.0.1'
 postgre_login     = 'postgres'
 postgre_password  = 'postgres'
 postgre_schema    = 'dwh'
 postgre_port      = 5435
-
-
-fetch = FetchApi('http://103.150.197.96:5005/api/v1/rekapitulasi_v2/jabar/harian?level=kab%22')
-df    = fetch.get_data()
-
-engine_mysql = connect_mysql(
-    host     = mysql_host,
-    user     = mysql_login,
-    password = mysql_password,
-    db       = mysql_schema,
-    port     = mysql_port
-)
 
 engine_postgre = connect_postgres(
     host     = postgre_host,
@@ -55,6 +42,9 @@ engine_postgre = connect_postgres(
     db       = postgre_schema,
     port     = postgre_port
 )
+
+fetch = FetchApi('http://103.150.197.96:5005/api/v1/rekapitulasi_v2/jabar/harian?level=kab%22')
+df    = fetch.get_data()
 
 df.to_sql(con=engine_mysql, name='covid_data',if_exists='replace', index=False)
 
@@ -66,6 +56,3 @@ transformer.create_dim_table('case')
 transformer.insert_daily('district')
 transformer.insert_daily('province')
 
-# print(data.head())
-# print(df[:1])
-# print(df)
